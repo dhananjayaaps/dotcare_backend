daed72d5a4b9fca3bee4a967c406bea879696040
dhananjayaaps <dhananjayaaps@gmail.com>
2024-08-28 15:22:57 +0530

    User data changed

--- src/main/java/com/dotcare/backend/entity/User.java
+++ src/main/java/com/dotcare/backend/entity/User.java
@@ -2,36 +2,41 @@ package com.dotcare.backend.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.Set;
 
-
+@Getter
+@Setter
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Getter
     private String username;
-    @Getter
-    @Setter
     private String password;
-    @Getter
     private String email;
-    @Getter
-    @Setter
     private boolean enabled;
-
-    @Getter
-    @Setter
+    private String first_name;
+    private String last_name;
+    private String nic;
+    private String password_confirmation;
+    private String phoneNumber;
+    private Boolean marketing_accept;
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
 
-    public User(String username, String encode, String email) {
+    public User(String username, String encode, String email, String first_name, String last_name, String nic, String phoneNumber, Boolean marketing_accept) {
         this.username = username;
         this.password = encode;
         this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.nic = nic;
+        this.phoneNumber = phoneNumber;
+        this.marketing_accept = marketing_accept;
     }
 
     public User() {
--- src/main/java/com/dotcare/backend/service/EmailService.java
+++ src/main/java/com/dotcare/backend/service/EmailService.java
@@ -6,6 +6,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendVerificationEmail(String recipientEmail, String verificationLink) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
