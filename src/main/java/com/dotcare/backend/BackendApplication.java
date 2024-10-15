27410bf7f1b14b0c71059dc2347fc84af6ba26e6
dhananjayaaps <dhananjayaaps@gmail.com>
2024-10-15 14:15:58 +0530

    added async

--- src/main/java/com/dotcare/backend/BackendApplication.java
+++ src/main/java/com/dotcare/backend/BackendApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication; // Make sure this import is correct
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync(proxyTargetClass=true)
 @SpringBootApplication
 public class BackendApplication {
 
